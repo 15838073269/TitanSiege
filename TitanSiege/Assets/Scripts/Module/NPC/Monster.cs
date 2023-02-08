9b1aedd4e3ee8c50339cc401e1648193e4d88ab2
xixin <304183153@qq.com>
2023-02-08 17:05:20 +0800

    新增unitask插件，怪物出生倒放溶解shader过渡

--- TitanSiege/Assets/Scripts/Module/NPC/Monster.cs
+++ TitanSiege/Assets/Scripts/Module/NPC/Monster.cs
@@ -6,15 +6,36 @@
 	功能：Nothing
 *****************************************************/
 
+using Cysharp.Threading.Tasks;
 using GF.Const;
 using GF.MainGame.Data;
+using System;
+using UnityEditor;
 using UnityEngine;
+using Progress = Cysharp.Threading.Tasks.Progress;
+
 namespace GF.MainGame.Module.NPC {
     public class Monster : NPCBase {
         private Vector3 m_Pos;//怪物的原始位置，用于战斗返回
         private Vector3 m_Rota;//怪物的原始旋转，用于战斗返回
         private Vector3 m_Scal;//怪物的原始缩放，用于放大技能后的恢复
+        public Material m_Material;
+        public async void OnEnable() {
+            //重置溶解特效
+            m_Material.SetFloat("_Cutoff",1);
+            //倒放溶解，显示怪物模型
+            ShowModel();
+        }
+        public async UniTask ShowModel() {
+            float _cut = 1f;
+            while (_cut >= 0) {
+                _cut -= 0.1f;
+                await UniTask.Delay(TimeSpan.FromSeconds(0.0618));
+                m_Material.SetFloat("_Cutoff", _cut);
+            }
+        }
         public override void InitNPCAnimaor() {
+            m_Material = transform.Find("Body").GetComponentInChildren<Renderer>().material;
             m_Nab = transform.GetComponent<MonsterAnimatorBase>();
             if (m_Nab == null) {
                 m_Nab = transform.gameObject.AddComponent<MonsterAnimatorBase>();
---
+++ TitanSiege/Assets/Scripts/Utilities/TestMonster.cs
@@ -0,0 +1,33 @@
+using Cysharp.Threading.Tasks;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMonster : MonoBehaviour
+{
+    public Material m_Material;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        m_Material = transform.Find("Body").GetComponentInChildren<Renderer>().material;
+    }
+    public void OnEnable() {
+        //重置溶解特效
+        m_Material.SetFloat("_Cutoff", 1);
+        tt();
+    }
+    public async UniTask tt() {
+        float _cut = 1f;
+        while (_cut>=0) {
+            _cut -= 0.1f;
+            await UniTask.Delay(TimeSpan.FromSeconds(0.0618));
+            m_Material.SetFloat("_Cutoff", _cut);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
