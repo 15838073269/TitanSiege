7322c1b6a5d89547c46c30eae0dfb963f6fcd13d
xixin <304183153@qq.com>
2023-02-17 17:49:25 +0800

    tt

--- TitanSiege/Assets/Scripts/Module/Fight/DamageFontModule.cs
+++ TitanSiege/Assets/Scripts/Module/Fight/DamageFontModule.cs
@@ -1,19 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
+
 using UnityEngine;
+using UnityEngine.Pool;
 using GF.Module;
 using GF.MainGame.UI;
-using UnityEngine.Pool;
+using GF.Unity.UI;
+using UnityEngine.SceneManagement;
+using GF.Unity.AB;
 
 namespace GF.MainGame.Module {
     public class DamageFontModule : GeneralModule {
-        GF.Pool.ClassObjectPool<DamageUIWidget> m_UIPool = new Pool.ClassObjectPool<DamageUIWidget>(40);
+        ObjectPool<DamageUIWidget> m_UIPool = new ObjectPool<DamageUIWidget>(CreateDamageUI,maxSize:20);
         public override void Create() {
             base.Create();
-
+            
         }
-
+        /// <summary>
+        /// 创建飘字预制体
+        /// </summary>
+        /// <returns></returns>
+        public static DamageUIWidget CreateDamageUI() {
+            HpUIPage hpuipage = UIManager.GetInstance.GetUI<HpUIPage>("HpUIPage");
+            GameObject go = ObjectManager.GetInstance.InstanceObject(AppConfig.DamageUIPath, setTranform: true, bClear: false, isfullpath: false, father: hpuipage.transform);
+            DamageUIWidget dui = go.GetComponent<DamageUIWidget>();
+            return dui;
+        }
+       
         public override void Release() {
             base.Release();
         }
--- TitanSiege/Assets/Scripts/UIModule/DamageUIWidget.cs
+++ TitanSiege/Assets/Scripts/UIModule/DamageUIWidget.cs
@@ -1,12 +1,42 @@
+using GF.Const;
 using GF.Unity.UI;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 namespace GF.MainGame.UI {
     public class DamageUIWidget : UIWidget {
+        public Text m_CrititalTxt;
+        public Text m_DodgeTxt;
+        public Text m_DamageTxt;
+        public Image m_RedTo;
+        public Image m_Red;
         protected override void OnOpen(object args = null) {
             base.OnOpen(args);
-
+            m_CrititalTxt.gameObject.SetActive(false);
+            m_DodgeTxt.gameObject.SetActive(false);
+            m_DamageTxt.gameObject.SetActive(false);
+        }
+        public void SetAndShow(int damage,DamageType damagetype) {
+            switch (damagetype) {
+                case DamageType.none:
+                    m_DamageTxt.text = damage + "";
+                    m_DamageTxt.gameObject.SetActive(true);
+                    break;
+                case DamageType.shangbi:
+                    m_DodgeTxt.gameObject.SetActive(true);
+                    break;
+                case DamageType.baoji:
+                    m_CrititalTxt.text = $"±©»÷{damage.ToString()}";
+                    m_CrititalTxt.gameObject.SetActive(true);
+                    break;
+                default:
+                    m_DamageTxt.text = damage + "";
+                    m_DamageTxt.gameObject.SetActive(true);
+                    break;
+            }
+            
         }
         protected override void OnUpdate() {
             base.OnUpdate();
